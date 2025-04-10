7970c4509d6b8df9b2843ffb220d00c00a25f5c5
Victor Hugo Maran <142737443+VictorHugoMaran@users.noreply.github.com>
2025-04-10 17:17:03 +0000

    opa...

---
+++ primeiro.py
@@ -0,0 +1,11 @@
+nome1 = input("Digite o nome do primeiro usuário: ")
+nome2 = input("Digite o nome do segundo usuário: ")
+
+print(f"Ola, {nome1}! Tudo bem?")
+print(f"Ola, {nome2}! Tudo bem?")
+
+idade1 = int(input("Digite a idade do primeiro usuário: "))
+idade2 = int(input("Digite a idade do segundo usuário: "))
+
+print(f"A idade do {nome1} é {idade1} anos.")
+print(f"A idade do {nome2} é {idade2} anos.")
